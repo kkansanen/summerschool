2f23278ddfdd49d77989d3f0c613017b2eb1462a
kansanen <kasperkansanen@gmail.com>
2017-06-28 12:47:53 +0300

    added an if statement

--- exercises/programming/c/control-structures/fibonacci.c
+++ exercises/programming/c/control-structures/fibonacci.c
@@ -11,10 +11,14 @@ int main(int argc, char *argv[])
    // initialize variables and write the while loop
    // Remember to update all variables within the loop
 	while (f2<100){
-		f2=f1+f0;	
+		f2=f1+f0;
+		if (f2>=100){
+		break;
+		}else{	
 		printf("%d\n",f2);
 		f0 = f1;
 		f1 = f2;
+		}
 	}
    return 0;
 }
