659384145a58e322fb84c886ac413458d0c3062c
kkansanen <kasperkansanen@gmail.com>
2017-06-28 19:29:43 +0300

    codes for 5d

---
+++ exercises/programming/c/datastructures-functions-heat/5d.c
@@ -0,0 +1,73 @@
+#define NX 0.01
+#define NY 0.01
+
+#define DX 0.01
+#define DY 0.01
+	
+typedef struct {
+		
+	int nx, ny;
+	double dx, dy;
+	double dx2;
+	double dy2;
+	double array[nx+2][ny+2];
+		
+		
+} tempfield;
+	
+	
+void initialize_field (tempfield *f, int nx, int ny, double dx, double dy){
+	
+	int i, j;
+	
+	f->nx = nx;
+    f->ny = ny;
+    f->dx = dx;
+    f->dy = dy;
+    f->dx2 = dx*dx;
+    f->dy2 = dy*dy;
+	
+	for (i = 1; i < f->nx+1; i++) {
+        for (j = 1; j < ny+1; j++) {
+            f->array[i][j] = 0.0;
+        }
+    }
+	
+	// Initial conditions for left and right
+    for (i = 0; i < nx+2; i++) {
+        f->array[i][0] = 20.0;
+        f->array[i][f->ny+1] = 70.0;
+    }
+    // up and down
+    for (j = 0; j < f->ny+2; j++) {
+        f->array[0][j] = 85.0;
+        f->array[f->nx+1][j] = 5.0;
+    }
+}	
+	
+void laplacian (tempfield *f){
+	
+	int i, j;
+	double laplacian[f->nx+2][f->ny+2]
+	
+	for (i=1; i<f->nx+1; i++){
+		for (j=1; j<f->ny+1; j++){
+			laplacian[i][j] = (f->array[i-1][j]-2 * f->array[i][j]+ f->array[i+1][j])/(f->dx2) + (f->array[i][j-1]-2 * f->array[i][j]+f->array[i][j+1])/(f->dy2);
+		}
+	}
+	
+	for (i = 1; i < f->nx + 1; i++){
+        for (j = 1; j < f->ny + 1; j++){
+            f->array[i][j] = laplacian[i][j];
+		}
+	}
+}
+	
+int main(void){	
+	tempfield f;
+	initialize_field(&f, NX, NY, DX, DY);
+	laplacian(&f);
+	
+	return 0;
+}
+
