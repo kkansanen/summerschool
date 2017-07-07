a2c722588c20890ff8c71d848ece9b5ff3fb91fc
kkansanen <kasperkansanen@gmail.com>
2017-07-07 12:26:29 +0300

    fight against trees continues, with fortran

--- exercises/programming/fortran/hello/hello.F90
+++ exercises/programming/fortran/hello/hello.F90
@@ -1,5 +1,16 @@
 program hello
   implicit none
-
+  real :: x, y
+  integer :: i = 5
+  integer :: j
+  real :: z(1000) = (/(j, j=1,1000)/)
   write (*,*) 'Hello world from Fortran!'
+  x = 2.0**i
+  y = 2.0**real(i)
+  write (*,*) '2.0**i = ', x
+  write (*,*) '2.0**real(i) = ', y
+  write (*,*) 'x+y = ', x+y
+  write (*,*) 'z(1 : 1000 : 5) = ', z ( 1 : 1000 : 5 )
+
+
 end program hello
--- exercises/programming/fortran/loops/loops.F90
+++ exercises/programming/fortran/loops/loops.F90
@@ -3,16 +3,25 @@ program loops
   ! TODO define parameters nx and ny
   ! TODO: define real-valued array A
   integer :: i, j
-
+  integer, parameter :: nx = 20, ny = 10
+  real :: a(0:nx+1, 0:ny+1)
   ! TODO initialize array A here
-
-
-
+  a = 65.0
+  !sides
+  do i=0, nx+1
+   a(i,0) = 20.0
+   a(i, ny+1) = 85.0
+  end do
+  !up and down
+  do j=0, ny+1
+   a(0, j) = 70.0
+   a(nx+1, j) = 5.0
+  end do
   !--------------------------------------------------
   ! Print out the array
   ! the ':' syntax means the whole row, see the Fortran arrays lecture
   do i = 0, nx+1
-     write(*, '(12F6.1)') A(i,:)
+     write(*, '(12F6.1)') a(i,:)
   end do
 
 end program loops
